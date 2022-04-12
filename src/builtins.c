a4679e8de5a8a82bd0e21a9dadf77e92951e73b9
Ben Tenzlinger <btenzlin@3-C-3.42heilbronn.de>
2022-04-12 12:16:13 +0200

    added cd (very unfinished) + pwd function

--- src/builtins.c
+++ src/builtins.c
@@ -1,6 +1,15 @@
 #include "../include/minishell.h"
 
-int	echo(char **cmd_line)
+//	do we want to include function name in command line argument
+//	e.g. from parser to echo: "echo -n hello world" or 
+//	"-n hello world"?
+
+/*	@brief	recode of linux echo function
+ *	@params	command line from parser
+ *	@return	if function succeeded
+ *	UNCLEAR	do backslash sequences have to be handled (without -e flag)?
+ */
+int	ft_echo(char **cmd_line)
 {
 	int	i;
 	int	is_flag;
@@ -23,5 +32,31 @@ int	echo(char **cmd_line)
 	}
 	if (!is_flag)
 		printf("\n");
+	return (EXIT_SUCCESS);
+}
+
+/*	@brief	changes current working directory of the shell
+ *	@params	command line IF POSSIBLE as string (as opposed to 2d array)
+ *	@return	if function scceeded
+ *	FIXME test when parser ready
+ */
+int	ft_cd(char *cmd_line)
+{
+	return (chdir(cmd_line));
+}
+
+/*	@brief	print name of the working directory
+ *	@return	if function scceeded
+ *	DECIDE	if we want to use set size array or dinamically allocate memory for path
+ */
+int	ft_pwd(void)
+{
+	char	*path;
+
+	path = getcwd(NULL, 0);
+	if (!path)
+		return (EXIT_FAILURE);
+	printf("%s\n", path);
+	free(path);
 	return (EXIT_SUCCESS);
 }
