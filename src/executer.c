12ca3bd34103a3574aa10c34853bb19d69e2e3a6
Ben Tenzlinger <btenzlin@2-B-9.42heilbronn.de>
2022-04-14 13:39:23 +0200

    added functions and executer for non-builtins(for testing)

--- src/executer.c
+++ src/executer.c
@@ -1,13 +1,46 @@
 #include "../include/minishell.h"
 
+static int	exec_not_builtin(char **cmd_line)
+{
+	pid_t	pid;
+	int		status;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		execvp(cmd_line[0], cmd_line); //FORBIDDEN FUNC: just for testing
+		printf("%s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	else if (pid < 0)
+		write(2, "Error\n", 6);
+	else
+		wait(&status);
+	return (0);
+}
+
 int	msh_executer(char *command)
 {
 	int		status;
-	// char	**cmd_line;
+	char	**cmd_line;
 
-	status = printf("%s\n", command);
-	// cmd_line = ft_split(command, ',');
-	// ft_echo(cmd_line);
-	// status = 1;
+	// status = printf("%s\n", command);
+	cmd_line = ft_split(command, ',');
+	if (!ft_strncmp(cmd_line[0], "echo", 4))
+		ft_echo(cmd_line);
+	else if (!ft_strncmp(cmd_line[0], "pwd", 3))
+		ft_pwd();
+	else if (!ft_strncmp(cmd_line[0], "cd", 2))
+		ft_cd(command);
+	else if (!ft_strncmp(cmd_line[0], "exit", 4))
+	{
+		write(1, "exit\n", 5);
+		return (0);
+	}
+	else
+	{
+		exec_not_builtin(cmd_line);
+	}
+	status = 1;
 	return (status);
 }
