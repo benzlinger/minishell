c2c81f04672c33fb12642b17a76e137f69e0f802
Ben Tenzlinger <btenzlin@2-B-9.42heilbronn.de>
2022-04-14 12:42:32 +0200

    removed ft_echo due to segfault

--- src/executer.c
+++ src/executer.c
@@ -3,11 +3,11 @@
 int	msh_executer(char *command)
 {
 	int		status;
-	char	**cmd_line;
+	// char	**cmd_line;
 
-	// status = printf("%s\n", command);
-	cmd_line = ft_split(command, ',');
-	ft_echo(cmd_line);
-	status = 1;
+	status = printf("%s\n", command);
+	// cmd_line = ft_split(command, ',');
+	// ft_echo(cmd_line);
+	// status = 1;
 	return (status);
 }
