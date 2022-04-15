2e5496798231aeb52b232c2125e36458f9f0d11c
rsiebert <rsiebert@student.42heilbronn.de>
2022-04-15 14:44:41 +0200

    check_EOF now covers prompt with only space ' '

--- src/utils_lexer.c
+++ src/utils_lexer.c
@@ -3,14 +3,25 @@
 /**
  * 	@brief	checks for EOF "signal" from ctrl+D and
  * 		exits the programm if an EOF was detected
+ * 		also check if string is only space ' '
  * 	@param	s: string from readline
  * 	@return	true is returned if s is not NULL (no EOF detected)
+ * 		true if string is not only space ' '
  */
 bool	ft_check_EOF(char *s)
 {
+	int	i;
+
 	if (s == NULL)
 		ft_exit(EOF);
-	return (true);
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] != ' ')
+			return (true);
+		i++;
+	}
+	return (false);
 }
 
 /**
