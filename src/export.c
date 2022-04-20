9fe2d031779d1fc91f719ff4c3a7b6c2a0c58e59
Ben Tenzlinger <btenzlin@3-C-3.42heilbronn.de>
2022-04-20 16:00:24 +0200

    fixed segfault + added new_var fct

--- src/export.c
+++ src/export.c
@@ -1,4 +1,4 @@
-#include "minishell.h"
+#include "../include/minishell.h"
 
 //what if list is empty
 
@@ -24,36 +24,20 @@ static int	show_vars(t_vars *head)
 	return (EXIT_SUCCESS);
 }
 
-/*	@brief	add env variable to list of existing vars
- *	@params	name of var, value of var (can be NULL), head of env variable list
- *	@return	if function succeeded
- */
-static int	append_var(char *name, char *value, t_vars *head)
+static t_vars	*new_var(char *name, char *value)
 {
-	t_vars	*tmp;
-	t_vars	*new;
+	t_vars	*head;
 
+	head = malloc(sizeof(t_vars));
 	if (!head)
-		return (EXIT_FAILURE);
-	tmp = find_var(name, head);
-	if (tmp)
-	{
-		if (value)
-			tmp->value = value;
-		return (EXIT_SUCCESS);
-	}
-	new = malloc(sizeof(t_vars));
-	if (!new)
-		return (EXIT_FAILURE);
-	tmp = head;
-	while (tmp->next)
-		tmp = tmp->next;
-	tmp->next = new;
-	new->name = name;
+		return (NULL);
+	head->name = name;
 	if (value)
-		new->value = value;
-	new->next = NULL;
-	return (EXIT_SUCCESS);
+		head->value = value;
+	else
+		head->value = NULL;
+	head->next = NULL;
+	return (head);
 }
 
 /*	@brief	find env variable in list by name
@@ -76,20 +60,39 @@ static t_vars	*find_var(char *name, t_vars *head)
 	return (NULL);
 }
 
-/*	@brief	free the whole list of env variables
- *	@params	head of env variable list
- *	@return	if node is found: node, else NULL
+/*	@brief	add env variable to list of existing vars
+ *	@params	name of var, value of var (can be NULL), head of env variable list
+ *	@return	if function succeeded
  */
-static void	free_vars(t_vars *head)
+static t_vars	*append_var(char *name, char *value, t_vars *head)
 {
 	t_vars	*tmp;
+	t_vars	*new;
 
-	while (head)
+	if (!head)
 	{
-		tmp = head->next;
-		free(head);
-		head = tmp;
+		head = new_var(name, value);
+		return (head);
 	}
+	tmp = find_var(name, head);
+	if (tmp)
+	{
+		if (value)
+			tmp->value = value;
+		return (head);
+	}
+	new = malloc(sizeof(t_vars));
+	if (!new)
+		return (NULL);
+	tmp = head;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = new;
+	new->name = name;
+	if (value)
+		new->value = value;
+	new->next = NULL;
+	return (head);
 }
 
 /*	@brief	show or create new env variable [with value]
@@ -97,16 +100,16 @@ static void	free_vars(t_vars *head)
  *	@return	if function scceeded
  *	FIXME	if env var name already exists (assign new value)
  */
-static int	ft_export(char **cmd_line, t_vars *head)
+t_vars	*ft_export(char **cmd_line, t_vars *head)
 {
-	int			i;
+	int	i;
 
-	if (cmd_line[1][0] == '=')
-		return (EXIT_FAILURE);
+	if (cmd_line[1] && cmd_line[1][0] == '=')
+		return (NULL);
 	if (!cmd_line[1])
 	{
-		if (!show_vars(head))
-			return (EXIT_FAILURE);
+		if (show_vars(head))
+			return (NULL);
 	}
 	else
 	{
@@ -115,15 +118,16 @@ static int	ft_export(char **cmd_line, t_vars *head)
 		{
 			if (cmd_line[i + 1] && cmd_line[i + 1][0] == '=')
 			{
-				append_var(cmd_line[i], cmd_line[i + 1], head);
+				head = append_var(cmd_line[i], cmd_line[i + 1], head);
 				i++;
 			}
 			else
-				append_var(cmd_line[i], NULL, head);
+				head = append_var(cmd_line[i], NULL, head);
 			i++;
 		}
 	}
-	return (EXIT_SUCCESS);
+	// print_vars(head);
+	return (head);
 }
 
 /*	@brief	delete variable from list
@@ -158,7 +162,7 @@ static t_vars	*del_var(char *name, t_vars *head)
  *	@params	command line, head of list
  *	@return	(new) head of list
  */
-static t_vars	*ft_unset(char** cmd_line, t_vars *head)
+t_vars	*ft_unset(char **cmd_line, t_vars *head)
 {
 	int	i;
 
