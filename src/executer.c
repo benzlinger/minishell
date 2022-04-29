2fa00a99f162211db6c53afc3cf49b5757198b60
Ben Tenzlinger <btenzlin@3-C-3.42heilbronn.de>
2022-04-29 12:59:13 +0200

    clean up

--- src/executer.c
+++ src/executer.c
@@ -82,16 +82,16 @@ int	msh_executer(t_data *data)
 		ft_export(data, exp_cmd);
 		free_2d_array(exp_cmd);
 	}
-	else if (!ft_strncmp(cmd_line[0], "exit", 4))
-	{
-		write(1, "exit\n", 5);
-		status = 0;
-	}
 	else if (!ft_strncmp(cmd_line[0], "unset", 5))
 	{
 		exp_cmd = export_cmd(data->command);
 		data->vars = ft_unset(exp_cmd, data->vars);
 		free_2d_array(exp_cmd);
+	}
+	else if (!ft_strncmp(cmd_line[0], "exit", 4))
+	{
+		write(1, "exit\n", 5);
+		status = 0;
 	}
 	else
 		exec_not_builtin(cmd_line, data->env_list);
