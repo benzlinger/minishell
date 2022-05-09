320027fcefa610832c6c0a5b693a1323344f50f3
Ben Tenzlinger <btenzlin@3-C-3.42heilbronn.de>
2022-05-09 14:34:42 +0200

    added exitstatus checker + moved functions to new file

--- src/utils_parser_dquote.c
+++ src/utils_parser_dquote.c
@@ -87,73 +87,6 @@ static char	*handle_quotes(char *s)
 	return (out);
 }
 
-static char	*replace_helper(char *s, char *exit, int new_size)
-{
-	char	*new;
-	int		i;
-	int		j;
-	int		k;
-
-	new = malloc(new_size + 1);
-	if (!new)
-		ft_error(strerror(errno));
-	i = 0;
-	j = 0;
-	while (s[i])
-	{
-		if (s[i] == '$' && s[i + 1] == '?')
-		{
-			k = 0;
-			while (exit[k])
-			{
-				new[j] = exit[k];
-				j++;
-				k++;
-			}
-			i += 2;
-		}
-		else
-		{
-			new[j] = s[i];
-			i++;
-			j++;
-		}
-	}
-	new[j] = '\0';
-	return (new);
-}
-
-static char	*replace_exitstatus(char *s, t_data *data)
-{
-	int		i;
-	int		j;
-	int		size;
-	char	*exit;
-	char	*new;
-
-	if (!s)
-		return (s);
-	i = 0;
-	j = 0;
-	exit = ft_itoa(data->exitstatus);
-	size = ft_strlen(exit);
-	while (s[i])
-	{
-		if (s[i] == '$' && s[i + 1] == '?')
-		{
-			j += size;
-			i++;
-		}
-		else
-			j++;
-		i++;
-	}
-	new = replace_helper(s, exit, j);
-	free(exit);
-	free(s);
-	return (new);
-}
-
 /**
  * 	@brief	handles double quotes; removes them and inserts envars
  * 	@param	s: pointer to literal token string
@@ -169,7 +102,8 @@ char	*type_dquote(char **s, t_data *data)
 		free(*s);
 		return (NULL);
 	}
-	out = replace_exitstatus(out, data);
+	if (has_exitstatus(out))
+		out = replace_exitstatus(out, data);
 	if (envar_exists(*s))
 		out = insert_envar(&out, data);
 	if (out == NULL)
