5d12e949d1240e33564459d97436ecdc5ec48004
Ben Tenzlinger <btenzlin@3-C-2.42heilbronn.de>
2022-05-26 15:33:05 +0200

    fixed checksum error

--- src/utils_executer_redirec.c
+++ src/utils_executer_redirec.c
@@ -62,6 +62,28 @@ static int	twod_elems(char **cmd_line)
 	return (i * 4);
 }
 
+int	countyy(char **cmd_line)
+{
+	int	i;
+	int	j;
+	int	count;
+
+	i = 0;
+	j = 0;
+	count = 0;
+	while (cmd_line[i] != NULL)
+	{
+		while (cmd_line[i][j] != '\0')
+		{
+			count++;
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	return (count);
+}
+
 // FIXME
 // somethings going wrong with grep and awk after a pipe
 char	*remove_redirec(char **cmd_line)
@@ -71,7 +93,7 @@ char	*remove_redirec(char **cmd_line)
 	int		j;
 	int		k;
 
-	cmd = ft_calloc(get_size_2d(cmd_line) + twod_elems(cmd_line), sizeof(char));
+	cmd = ft_calloc(countyy(cmd_line), sizeof(char));
 	if (cmd == NULL)
 		ft_error(strerror(errno));
 	i = 0;
@@ -101,6 +123,30 @@ char	*remove_redirec(char **cmd_line)
 	return (cmd);
 }
 
+void	remove_tmp(t_data *data)
+{
+	char	**cmd_line;
+	int		pid;
+
+	if (access(".tmp.txt", F_OK) == 0)
+	{
+		cmd_line = malloc(sizeof(char *) * 3);
+		cmd_line[0] = "/bin/rm";
+		cmd_line[1] = ".tmp.txt";
+		cmd_line[2] = NULL;
+		pid = fork();
+		if (pid == -1)
+			ft_error(strerror(errno));
+		else if (pid == 0)
+			execve(cmd_line[0], cmd_line, data->env_list);
+		else
+		{
+			wait(NULL);
+			free(cmd_line);
+		}
+	}
+}
+
 /**
  * 	@brief	handles redirections and heredocs
  * 	@param	cmd_line: the old 2d command line
@@ -117,5 +163,6 @@ char	**ft_redirec(char **cmd_line, t_data *data)
 	cmd = remove_redirec(cmd_line);
 	new_cmd_line = ft_split(cmd, ',');
 	free(cmd);
+	remove_tmp(data);
 	return (new_cmd_line);
 }
